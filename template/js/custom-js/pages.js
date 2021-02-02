4a05563311dbd65551d868f07d8f742cc646eacf
Matheus Reis <35343551+matheusgnreis@users.noreply.github.com>
2021-02-02 02:24:11 -0300

    chore: update new version

--- template/js/custom-js/pages.js
+++ template/js/custom-js/pages.js
@@ -1,13 +1,4 @@
 // Add your custom JavaScript for storefront pages here.
-if (window.innerWidth > 992) {
-  const menuDesk = document.getElementById('menu-novo')
-  const searchBarDesk = document.getElementById('search-bar')
-  const buttonsDesk = document.getElementById('headButtons')
-  const logoDesk = document.getElementById('coluna-logo')
-  logoDesk.classList.remove('col-lg-auto')
-  menuDesk.append(searchBarDesk)
-  menuDesk.append(buttonsDesk)
-}
 if (window.innerWidth < 991) {
   const searchFormMobile = document.getElementById('search-bar')
   const buttonSearchOpen = document.getElementById('mobile-search-btn')
@@ -20,3 +11,8 @@ if (window.innerWidth < 991) {
   menuMobile.before(searchFormMobile)
   menuList[position - 1].after(userMobile)
 }
+const buttonTo = document.getElementById('link-direct')
+const productB = document.querySelector('.product-card')
+productB.append(buttonTo)
+console.log(buttonTo)
+console.log(productB)
